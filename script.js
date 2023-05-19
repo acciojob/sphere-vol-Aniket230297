8406e80180b068b190d732f2d77d86fa86baf1cc
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-19 18:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius=document.getElementById("radius").value;
+	let volume=(4/3*3.14*(radius*radius*radius));
+    //let output=document.getElementById("volume").value;
+	//output.innerHTML= volume;
+	document.getElementById("volume").value=volume;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
